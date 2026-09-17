592eb5aa84fd01e49b0ad1fb9ec31a384b7c0c99
doomsday <intniogi@hotmail.com>
2026-09-17 12:32:53 +0300

    edited main page style for app name, edited addTask display style

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,5 @@
 
-
+// Create an instance of the addProjectContainer so i can insert a button bellow (addNewProjectBtn)
 const addProjectContainer = document.querySelector('.addProjectContainer')
 
 const addNewProjectBtn = document.createElement('button')
@@ -7,3 +7,10 @@ addNewProjectBtn.classList.add('projectAdd')
 addNewProjectBtn.innerHTML = 'Add Project'
 addProjectContainer.appendChild(addNewProjectBtn)
 
+// Create an instance of the taskDisplayTopPanel so i can insert a button bellow (addTaskBtn)
+const taskDisplayTopPanel = document.querySelector('.taskDisplayTopPanel')
+
+const addTaskBtn = document.createElement('button')
+addTaskBtn.classList.add('taskAdd')
+addTaskBtn.innerHTML = 'Add Task'
+taskDisplayTopPanel.appendChild(addTaskBtn)
