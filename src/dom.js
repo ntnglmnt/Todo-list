32a30fa85ba9f1e9547f6a4aa66404895117c677
doomsday <intniogi@hotmail.com>
2026-09-17 11:10:04 +0300

    created the add project button inside dom.js and stylized it in style.css

--- src/dom.js
+++ src/dom.js
@@ -1,1 +1,9 @@
-const 
+
+
+const addProjectContainer = document.querySelector('.addProjectContainer')
+
+const addNewProjectBtn = document.createElement('button')
+addNewProjectBtn.classList.add('projectAdd')
+addNewProjectBtn.innerHTML = 'Add Project'
+addProjectContainer.appendChild(addNewProjectBtn)
+
